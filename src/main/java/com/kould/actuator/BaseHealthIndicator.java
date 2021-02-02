bd5ac0346d7e053b369a09623cf02ca13384cae7
Kould <2815680434@qq.com>
2021-02-03 05:11:33 +0800

    1、整合Spring actuator包，並提供info與health功能

---
+++ src/main/java/com/kould/actuator/BaseHealthIndicator.java
@@ -0,0 +1,18 @@
+package com.kould.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = 100 ;
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build() ;
+        }
+        return Health.up().build() ;
+    }
+}
